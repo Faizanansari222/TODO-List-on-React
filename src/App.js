65f54788f90fa8b65402268539f19c89f91eca0a
Faizanansari222 <faizanrazaansari07@gmail.com>
2023-12-24 00:27:20 +0500

    First Assigenment on React to creat TODO List

--- src/App.js
+++ src/App.js
@@ -1,24 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 function App() {
+  const [list, setList] = useState([]);
+  const [inputValue, setInputValue] = useState();
+  const [editMode, setEditMode] = useState(false);
+  const [currentIndex, setCurrentIndex] = useState();
+
+  function addFun() {
+    const copyList = [...list];
+    copyList.push(inputValue);
+    setList(copyList);
+    setInputValue("");
+  }
+
+  function deleteItem(index) {
+    const copyList = [...list];
+    copyList.splice(index, 1);
+    setList(copyList);
+  }
+
+  function editItem(index) {
+    const itemValue = list[index];
+    setInputValue(itemValue);
+    setEditMode(true);
+    setCurrentIndex(index);
+  }
+
+  function updateFun() {
+    const copyList = [...list];
+    copyList[currentIndex] = inputValue;
+    setList(copyList);
+
+    setEditMode(false);
+    setInputValue("");
+  }
+
+  function textUpdate(e) {
+    const value = e.target.value;
+    setInputValue(value);
+  }
   return (
+  <>
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <input
+          placeholder="enter any text"
+          onChange={textUpdate}
+          value={inputValue}
+        />
+        {editMode ? (
+          <button onClick={updateFun}>Update</button>
+        ) : (
+          <button onClick={addFun}>add</button>
+        )}
+
+        <ul>
+          {list.map(function (item, index) {
+            return (
+              <li>
+                {item}
+                <button onClick={() => editItem(index)}>Edit</button>
+                <button onClick={() => deleteItem(index)}>Delete</button>
+              </li>
+            );
+          })}
+        </ul>
       </header>
     </div>
+    </>
   );
 }
 
